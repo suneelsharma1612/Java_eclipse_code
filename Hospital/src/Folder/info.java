e8e0fddc16a43def287a567e181b920ca8ad2010
suneelsharma1612 <37187219+suneelsharma1612@users.noreply.github.com>
2019-01-07 13:59:26 +0530

    Create info.java

---
+++ Hospital/src/Folder/info.java
@@ -0,0 +1,8 @@
+package Folder;
+public interface info{
+	String DB_driver="oracle.jdbc.OracleDriver";
+	String DB_url="jdbc:oracle:thin:@localhost:1521:XE";
+	String DB_username="";
+	String DB_password="";
+			
+} 
