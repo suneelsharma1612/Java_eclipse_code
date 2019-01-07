1fea798395fe0a5e81ac51f3609640dc3125f29c
suneelsharma1612 <37187219+suneelsharma1612@users.noreply.github.com>
2019-01-07 14:01:53 +0530

    Create DataBean.java

---
+++ JdbcBeansDemo/src/mybean/DataBean.java
@@ -0,0 +1,10 @@
+package mybean;
+
+public interface DataBean
+{
+	String DB_url="oracle.jdbc.OracleDriver";
+	String DB_driver_url="jdbc:oracle:thin:@localhost:1521:XE";
+	String DB_username="SpiJava";
+	String DB_password="root";
+	
+}
