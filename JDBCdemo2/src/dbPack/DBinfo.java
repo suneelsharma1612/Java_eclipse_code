eac84ced552eb299ea4d83adb7a804d73c44a858
suneelsharma1612 <37187219+suneelsharma1612@users.noreply.github.com>
2019-01-07 14:03:54 +0530

    Create DBinfo.java

---
+++ JDBCdemo2/src/dbPack/DBinfo.java
@@ -0,0 +1,8 @@
+package dbPack;
+
+public interface DBinfo{
+	String DB_driver="oracle.jdbc.OracleDriver";
+	String DB_url="jdbc:oracle:thin:@localhost:1521:XE";
+	String DB_username="spiJava";
+	String DB_password="root";
+}
